e271dd5d9de3479c33c1b8164e7a4464566abc42
h-alex2 <glowhyun1@gmail.com>
2022-09-05 15:19:38 +0900

    move test files

--- test/done/averagePair.test.js
+++ test/done/averagePair.test.js
@@ -1,7 +1,7 @@
 import { expect } from "chai";
-import averagePair from "../practice/pointer/averagePair";
+import averagePair from "../../practice/pointer/averagePair";
 
-describe("averagePair", () => {
+describe.skip("averagePair", () => {
   it("음수, 양수 상관없이 적용되어야 한다.", () => {
     expect(averagePair([1,2,3],2.5)).to.equal(true);
     expect(averagePair([1,3,3,5,6,7,10,12,19],8)).to.equal(true);
