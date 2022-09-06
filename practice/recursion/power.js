7266e0e3931d508e6842c47cc15c81accf2f8e1a
h-alex2 <glowhyun1@gmail.com>
2022-09-06 12:43:42 +0900

    fix: arguments name

--- practice/recursion/power.js
+++ practice/recursion/power.js
@@ -1,10 +1,9 @@
-// Write a function called power which accepts a base and an exponent.
-// The function should return the power of the base to the exponent.
+// *Write a function called power which accepts a base and an exponent. The function should return the power of the base to the exponent.
 // This function should mimic the functionality of Math.pow()
 // - do not worry about negative bases and exponents.
 
-export default function power(num1, num2) {
-  if (num2 === 0) {
+export default function power(base, exponent) {
+  if (exponent === 0) {
     return 1;
   }
 
@@ -12,7 +11,7 @@ export default function power(num1, num2) {
   //   return num1 / power(num1, num2 + 1);
   // }
 
-  return num1 * power(num1, num2 - 1);
+  return base * power(base, exponent - 1);
 }
 
 // 시간 된다면 num2가 음수일 경우나 제곱근 구하는 경우도 작성해보기
